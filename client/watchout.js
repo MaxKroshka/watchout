bdc208702558320288ea8318b840e340be571982
MaxKroshka <max.kroshka@gmail.com>
2016-01-22 16:00:56 -0800

    Add svg board object and add circles at random using D3

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,36 @@
 // start slingin' some d3 here.
+
+
+var gameBoard = {
+  height: 400,
+  width: 600
+};
+
+
+
+
+var Enemy = function(x,y){
+  this.x = x;
+  this.y = y;
+};
+
+var enemies = Array.apply(null, Array(10)).map(function(){
+  return new Enemy(Math.floor(Math.random() * gameBoard.width), Math.floor(Math.random() * gameBoard.height));
+});
+
+// D3 time 
+
+// put down the svg board
+d3.select('.board')
+  .append('svg')
+  .attr('height', gameBoard.height)
+  .attr('width', gameBoard.width)
+  .attr('class', 'board')
+
+// add circles at random on svg board
+d3.select('svg.board').selectAll('circle')
+  .data(enemies)
+  .enter()
+  .append('circle')
+  .attr('cx', function(data) {return data.x})
+  .attr('cy',function(data) {return data.y});
